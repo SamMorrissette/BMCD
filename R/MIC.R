82e3a803d1f905897b28a5e4e1c9afd4cde73337
SamMorrissette <samuel.morrissette01@gmail.com>
2022-05-31 09:19:22 -0500

    add MIC for multiple dimensions

--- R/MIC.R
+++ R/MIC.R
@@ -3,97 +3,18 @@
 #' @importFrom LaplacesDemon dinvwishart
 #'
 
-MIC <- function(distances, bmcd_MCMC_list, priors, p, G) {
-  mcmc <- bmcd_MCMC_list
-
-  eps_star <- colMeans(mcmc$eps_list)
-  mu_star <- Reduce("+", mcmc$mu_list) / length(mcmc$mu_list)
-  T_star <- Reduce("+", mcmc$T_list) / length(mcmc$T_list)
-
-  # Calculate pi(Lambda*) ----------------------------------------------------
-
-  ## Epsilon
-  pr_eps <- gtools::ddirichlet(eps_star, rep(1, length(eps_star)))
-
-  ## Mu_j
-  pr_mu <- rep(NA, G)
-  for (i in 1:G) {
-    pr_mu[i] <- mvtnorm::dmvnorm(mu_star[,i], mean = priors$prior_mean[,i], sigma = matrix(T_star[,,i], nrow = p, ncol = p))
-  }
-
-  ## T_j
-  pr_T <- rep(NA, G)
-  for (i in 1:G) {
-    pr_T[i] <- LaplacesDemon::dinvwishart(T_star[,,i], nu = priors$prior_alpha, S = priors$prior_Bj[,,i], log = FALSE) # Change to log = TRUE?
-  }
-
-  pi_Lambda <- pr_eps * pr_mu * pr_T
-
-
-
-  # Calculate pi(X_pG | Lambda*) --------------------------------------------
-
-  X <- Reduce("+", mcmc$X_list) / length(mcmc$X_list)
-  n <- nrow(X)
+MIC <- function(distances, X_out, bmcd_MCMC_list, priors, p, min_G, max_G) {
+  mics <-rep(NA, max_G - min_G + 1)
+  n <- nrow(distances)
   m <- n*(n-1) / 2
 
-  x_densities <- rep(NA, n)
-  for (i in 1:n) {
-    total = 0
-    for (comp in 1:G) {
-      total = total + (eps_star[comp] * mvtnorm::dmvnorm(X[i,],
-                                                         mean = mu_star[,comp],
-                                                         sigma = matrix(T_star[,,comp], nrow = p, ncol = p)))
-    }
-    x_densities[i] <- total
-  }
+  # Calculate last term -----------------------------------------------------
+  A <- 0
+  for (q in 1:p) {
+    X <- X_out[[q]]
 
-
-  # Calculate pi(Lambda* | X_pG) --------------------------------------------
-
-  #n_star <- colMeans(mcmc$n_list)
-  z_star <- Reduce("+", mcmc$z_list) / length(mcmc$z_list)
-  K_star <- apply(z_star, 1, which.max)
-  n_star <- table(K_star)
-
-  ## Calculate pi(eps* | K)
-
-  pi_eps <- gtools::ddirichlet(eps_star, n_star + 1)
-
-  ## Calculate the product
-  pi_mu <- rep(NA, G)
-  pi_T <- rep(NA, G)
-
-  for (i in 1:G) {
-    ## Calculate pi(mu_j* | others)
-    x_j = X[which(K_star==i), , drop = FALSE]
-    x_j_bar = colMeans(x_j)
-    comp_mean = ((n_star[i] * x_j_bar) + priors$prior_mean[,i]) / (n_star[i] + 1)
-    comp_sigma = matrix(T_star[,,i] / (n_star[i] + 1), ncol = p, nrow = p)
-    pi_mu[i] <- mvtnorm::dmvnorm(mu_star[,i], mean = comp_mean, sigma = comp_sigma)
-
-    ## Calculate S_j
-    centered_x <- sweep(x_j, 2, mu_star[,i])
-    S_j <- 0
-    for (q in 1:nrow(centered_x)) {
-      S_j = S_j + (centered_x[q, ] %*% t(centered_x[q,]))
-    }
-
-    ## Calculate pi(T_j* | others)
-    pi_T[i] <- LaplacesDemon::dinvwishart(T_star[,,i],
-                                          nu = priors$prior_alpha + (n_star[i]/2),
-                                          S = priors$prior_Bj[,,i] + (S_j / 2))
-  }
-  expectation <- mean(pi_eps * prod(pi_mu * pi_T))
-
-  SSR <- sum((as.matrix(dist(X)) - distances)^2) / 2
-
-  if (p == 1) {
-    MIC <- ((m-2) * SSR) - (2 * log(prod(x_densities) * prod(pi_Lambda) / expectation))
-  } else if (p > 1) {
     # Calculate Hn ------------------------------------------------------------
     Hn = -((n + 1) * log(pi)) + (2 * log(gamma((n+1) / 2)))
-
 
     # Second term -------------------------------------------------------------
     centered_X <- sweep(X, 2, colMeans(X))
@@ -101,17 +22,157 @@ MIC <- function(distances, bmcd_MCMC_list, priors, p, G) {
     for (j in 1:n) {
       S = S + centered_X[j,] %*% t(centered_X[j,])
     }
-    -n * log((diag(S)[p]) / n)
 
     # Third term --------------------------------------------------------------
 
+    R <- vector("list", length = q)
+    for (i in 1:q) {
+      X_temp <- X_out[[i]]
+      R[[i]] <- t(X_temp) %*% X_temp
+    }
 
-    ### In order to calculate r_j, I need all dimensions of X (run edited_bmdsMCMC for dimensions 1:p)
-    for (j in 1:(p-1)) {
+    if (q >= 2) {
+      r <- rep(NA, q-1)
+      total <- 0
+      for (j in 1:(q-1)) {
+        r[j] <- diag(R[[q]])[j] / diag(R[[q-1]])[j]
+        total <- total + log(r[j]) + ((n+1) * log(n+1) / (n+r[j]))
+      }
+    } else {
+      total <- 0
+    }
+    A = Hn - (n * log((diag(S)[q]) / n)) + total
+  }
 
+  for (index in 1:(max_G - min_G + 1)) {
+    mcmc <- bmcd_MCMC_list[[index]]
+    G <- mcmc$G
+    prior_G <- priors[[index]]
+
+    eps_star <- colMeans(mcmc$eps_list)
+    mu_star <- Reduce("+", mcmc$mu_list) / length(mcmc$mu_list)
+    T_star <- Reduce("+", mcmc$T_list) / length(mcmc$T_list)
+
+    # Calculate pi(Lambda*) ----------------------------------------------------
+
+    ## Epsilon
+    pr_eps <- gtools::ddirichlet(eps_star, rep(1, length(eps_star)))
+
+    ## Mu_j
+    pr_mu <- rep(NA, G)
+    for (i in 1:G) {
+      pr_mu[i] <- mvtnorm::dmvnorm(mu_star[,i], mean = prior_G$prior_mean[,i], sigma = matrix(T_star[,,i], nrow = p, ncol = p))
+    }
+
+    ## T_j
+    pr_T <- rep(NA, G)
+    for (i in 1:G) {
+      tryCatch({
+        pr_T[i] <<- LaplacesDemon::dinvwishart(T_star[,,i],
+                                              nu = prior_G$prior_alpha,
+                                              S = prior_G$prior_Bj[,,i]) # Change to log = TRUE?
+      }, error = function(e) {
+        diag(prior_G$prior_Bj[,,i]) <- diag(prior_G$prior_Bj[,,i]) + 1e-05
+        pr_T[i] <<- LaplacesDemon::dinvwishart(T_star[,,i],
+                                              nu = prior_G$prior_alpha,
+                                              S = prior_G$prior_Bj[,,i]) # Change to log = TRUE?
+      })
+    }
+
+    pi_Lambda <- pr_eps * pr_mu * pr_T
+
+    # Calculate pi(X_pG | Lambda*) --------------------------------------------
+
+    X <- Reduce("+", mcmc$X_list) / length(mcmc$X_list)
+
+
+    x_densities <- rep(NA, n)
+    for (i in 1:n) {
+      total = 0
+      for (comp in 1:G) {
+        total = total + (eps_star[comp] * mvtnorm::dmvnorm(X[i,],
+                                                           mean = mu_star[,comp],
+                                                           sigma = matrix(T_star[,,comp], nrow = p, ncol = p)))
+      }
+      x_densities[i] <- total
     }
 
 
+    # Calculate pi(Lambda* | X_pG) --------------------------------------------
 
+    #n_star <- colMeans(mcmc$n_list)
+    z_star <- Reduce("+", mcmc$z_list) / length(mcmc$z_list)
+    K_star <- apply(z_star, 1, which.max)
+    n_star <- table(factor(K_star, levels = 1:G))
+
+    ## Calculate pi(eps* | K)
+
+    pi_eps <- gtools::ddirichlet(eps_star, n_star + 1)
+
+    ## Calculate the product
+    pi_mu <- rep(NA, G)
+    pi_T <- rep(NA, G)
+
+    for (i in 1:G) {
+      ## Calculate pi(mu_j* | others)
+      x_j = X[which(K_star==i), , drop = FALSE]
+      if (nrow(x_j) >= 1) {
+        x_j_bar = colMeans(x_j)
+        comp_mean = ((n_star[i] * x_j_bar) + prior_G$prior_mean[,i]) / (n_star[i] + 1)
+        comp_sigma = matrix(T_star[,,i] / (n_star[i] + 1), ncol = p, nrow = p)
+        pi_mu[i] <- mvtnorm::dmvnorm(mu_star[,i], mean = comp_mean, sigma = comp_sigma)
+
+        ## Calculate S_j
+        centered_x <- sweep(x_j, 2, mu_star[,i])
+        S_j <- 0
+        for (q in 1:nrow(centered_x)) {
+          S_j = S_j + (centered_x[q, ] %*% t(centered_x[q,]))
+        }
+
+        ## Calculate pi(T_j* | others)
+        Sigma <- prior_G$prior_Bj[,,i] + (S_j / 2)
+
+        tryCatch({
+          pi_T[i] <<- LaplacesDemon::dinvwishart(T_star[,,i],
+                                                nu = prior_G$prior_alpha + (n_star[i]/2),
+                                                S = Sigma)
+        }, error = function(e) {
+          diag(Sigma) <- diag(Sigma) + 1e-05
+          pi_T[i] <<- LaplacesDemon::dinvwishart(T_star[,,i],
+                                                 nu = prior_G$prior_alpha + (n_star[i]/2),
+                                                 S = Sigma) # Change to log = TRUE?
+        })
+      } else {
+        pi_mu[i] <- mvtnorm::dmvnorm(mu_star[,i],
+                                     mean = prior_G$prior_mean[,i],
+                                     sigma = matrix(T_star[,,i], ncol = p, nrow = p))
+        tryCatch({
+          pi_T[i] <<- LaplacesDemon::dinvwishart(T_star[,,i],
+                                                nu = prior_G$prior_alpha,
+                                                S = prior_G$prior_Bj[,,i]) # Change to log = TRUE?
+        }, error = function(e) {
+          diag(prior_G$prior_Bj[,,i]) <- diag(prior_G$prior_Bj[,,i]) + 1e-05
+          pi_T[i] <<- LaplacesDemon::dinvwishart(T_star[,,i],
+                                                nu = prior_G$prior_alpha,
+                                                S = prior_G$prior_Bj[,,i]) # Change to log = TRUE?
+        })
+      }
+    }
+    expectation <- mean(pi_eps * prod(pi_mu * pi_T))
+
+    SSR <- sum((as.matrix(dist(X)) - distances)^2) / 2
+
+    print(paste("SSR:", SSR))
+    print(paste("X densities:", sum(log(x_densities))))
+    print(paste("pi_Lambda densities:", sum(log(pi_Lambda))))
+    print(paste("expectation:", log(expectation)))
+    print((2 * (sum(log(x_densities)) + sum(log(pi_Lambda)) - log(expectation))))
+
+    if (p == 1) {
+      mics[index] <- ((m-2) * SSR) - (2 * (sum(log(x_densities)) + sum(log(pi_Lambda)) - log(expectation)))
+    } else if (p > 1) {
+      mics[index] <- ((m-2) * SSR) - (2 * (sum(log(x_densities)) + sum(log(pi_Lambda)) - log(expectation))) + A
+    }
   }
+  return(mics)
 }
--- R/bmcd.R
+++ R/bmcd.R
@@ -1,43 +1,63 @@
 #' @importFrom mclust Mclust mclustBIC
 #' @export
 
-bmcd <- function(distances, p, G, bmds_iter, bmds_burn, bmcd_iter, bmcd_burn, labelswitch_iter) {
+bmcd <- function(distances, max_p, min_G, max_G, bmds_iter, bmds_burn, bmcd_iter, bmcd_burn, labelswitch_iter) {
   n <- nrow(distances)
   m <- n*(n-1) / 2
 
   # Obtain an initial guess for X using BMDS algorithm ----------------------
 
-  bmds_out <- edited_bmdsMCMC(DIST = distances, p = p, nwarm = bmds_burn, niter = bmds_iter) # Doesn't include delta matrix (reduce memory required)
-  X_est <- bmds_out$x_bmds
+  X_out <- vector("list", length = max_p)
+  sigma_sq_out <- vector("list", length = max_p)
+  for (i in 1:max_p) {
+    # Doesn't include delta matrix (reduce memory required)
+    temp_bmds <- edited_bmdsMCMC(DIST = distances, p = i, nwarm = bmds_burn, niter = bmds_iter)
+    X_out[[i]] <- temp_bmds$x_bmds
+    sigma_sq_out[[i]] <- temp_bmds$e_sigma
+  }
+
+  # Calculate MDSIC to choose best p ----------------------------------------
+  mdsics <- edited_MDSIC(distances, X_out)
+  p <- which.min(mdsics)
+  print(p)
+
+  X_est <- X_out[[p]]
 
   # Set initial parameter values using GMM  ---------------------------------
 
-  mclust_out <- Mclust(X_est, G = G) # Fit GMM
+  out <- priors <- vector("list", length = max_G - min_G + 1)
 
-  sigma_sq <- bmds_out$e_sigma # Posterior mean of sigma squared (measurement error) from BMDS algorithm
-  eps_init <- mclust_out$parameters$pro # Vector of component mixing proportions
-  mu_init <- mclust_out$parameters$mean # Vector of component means
-  T_init <- mclust_out$parameters$variance$sigma # List of component covariance matrices
-  z_init <- mclust_out$z # Matrix of cluster assignment probabilities
+  ind <- 1
 
-  ## Accounting for one dimension
-  mu_init <- matrix(mu_init, nrow = p, ncol = G)
-  T_init <- array(T_init, c(p,p,G))
+  for (G in min_G:max_G) {
+    mclust_out <- Mclust(X_est, G = G) # Fit GMM
+    print(mclust_out)
 
-  # Set priors --------------------------------------------------------------
+    sigma_sq <- sigma_sq_out[[p]] # Posterior mean of sigma squared (measurement error) from BMDS algorithm
+    eps_init <- mclust_out$parameters$pro # Vector of component mixing proportions
+    mu_init <- mclust_out$parameters$mean # Vector of component means
+    T_init <- mclust_out$parameters$variance$sigma # List of component covariance matrices
+    z_init <- mclust_out$z # Matrix of cluster assignment probabilities
 
-  priors <- setPriors(distances, X_est, mclust_out$classification, p, G, n, m)
+    ## Accounting for one dimension
+    mu_init <- matrix(mu_init, nrow = p, ncol = G)
+    T_init <- array(T_init, c(p,p,G))
 
+    # Set priors --------------------------------------------------------------
 
-  # Initialize MCMC lists ---------------------------------------------------
-  init_params <- list(sigma_sq, eps_init, mu_init, T_init, z_init)
-  mcmc_list <- initLists(init_params, X_est, mclust_out$classification, p, G, n, bmcd_iter)
+    priors[[ind]] <- setPriors(distances, X_est, mclust_out$classification, p, G, n, m)
 
+    # Initialize MCMC lists ---------------------------------------------------
+    init_params <- list(sigma_sq, eps_init, mu_init, T_init, z_init)
+    mcmc_list <- initLists(init_params, X_est, mclust_out$classification, p, G, n, bmcd_iter)
 
-  # Run MCMC ----------------------------------------------------------------
-  out <- bmcdMCMC(distances, mcmc_list, priors, p, G, n, m, bmcd_iter, bmcd_burn, labelswitch_iter)
-
+    # Run MCMC ----------------------------------------------------------------
+    out[[ind]] <- bmcdMCMC(distances, mcmc_list, priors[[ind]], p, G, n, m, bmcd_iter, bmcd_burn, labelswitch_iter)
+    out[[ind]]$G <- G
+    ind <- ind + 1
+  }
 
   # Calculate MIC -----------------------------------------------------------
-  #calc_MIC <- MIC(distances, out, priors, p, G)
+  calc_MIC <- MIC(distances, X_out, out, priors, p, min_G, max_G)
+  print(calc_MIC)
 }
