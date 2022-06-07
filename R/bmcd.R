abec1afc193b4127402f4588e5b5ca7116c6b2fa
SamMorrissette <samuel.morrissette01@gmail.com>
2022-06-07 10:29:04 -0500

    add parallel

--- R/bmcd.R
+++ R/bmcd.R
@@ -1,7 +1,11 @@
 #' @importFrom mclust Mclust mclustBIC
+#' @importFrom foreach %dopar% foreach
+#' @importFrom iterators icount
+#' @importFrom doParallel registerDoParallel
 #' @export
 
-bmcd <- function(distances, max_p, min_G, max_G, bmds_iter, bmds_burn, bmcd_iter, bmcd_burn, labelswitch_iter) {
+bmcd <- function(distances, max_p, min_G, max_G, bmds_iter, bmds_burn, bmcd_iter, bmcd_burn, labelswitch_iter,
+                 parallel = FALSE, num_cores = NULL) {
   n <- nrow(distances)
   m <- n*(n-1) / 2
 
@@ -9,13 +13,29 @@ bmcd <- function(distances, max_p, min_G, max_G, bmds_iter, bmds_burn, bmcd_iter
 
   X_out <- vector("list", length = max_p)
   sigma_sq_out <- vector("list", length = max_p)
-  for (i in 1:max_p) {
-    # Doesn't include delta matrix (reduce memory required)
-    temp_bmds <- edited_bmdsMCMC(DIST = distances, p = i, nwarm = bmds_burn, niter = bmds_iter)
-    X_out[[i]] <- temp_bmds$x_bmds
-    sigma_sq_out[[i]] <- temp_bmds$e_sigma
-    print(i)
+
+  if (parallel == FALSE) {
+    for (i in 1:max_p) {
+      # Doesn't include delta matrix (reduce memory required)
+      temp_bmds <- edited_bmdsMCMC(DIST = distances, p = i, nwarm = bmds_burn, niter = bmds_iter)
+      X_out[[i]] <- temp_bmds$x_bmds
+      sigma_sq_out[[i]] <- temp_bmds$e_sigma
+      print(i)
+    }
+  } else if (parallel == TRUE & num_cores == 0) {
+    stop("num_cores = 0")
+  } else if (parallel == TRUE & num_cores > 0) {
+    doParallel::registerDoParallel(cores=num_cores)
+    comb <- function(x, ...) {
+      lapply(seq_along(x),
+             function(i) c(x[[i]], lapply(list(...), function(y) y[[i]])))
+    }
+    out_list <- foreach::foreach(j=1:max_p, .packages ="BMCD", .combine='comb', .multicombine=TRUE, .init=list(list(), list())) %dopar% {
+      output <- edited_bmdsMCMC(data, j, nwarm = bmds_burn, niter = bmds_iter)
+      list(output$x_bmds, output$e_sigma)
+    }
   }
+
 
   # Calculate MDSIC to choose best p ----------------------------------------
   mdsics <- edited_MDSIC(distances, X_out)
